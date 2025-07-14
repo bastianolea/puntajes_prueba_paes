d64ded77ab73a3d450c68eb16715964e401d5411
bastianolea <bastianolea@gmail.com>
2025-07-14 15:04:10 -0400

    datos

--- paes_procesar.R
+++ paes_procesar.R
@@ -10,37 +10,20 @@ paes |> glimpse()
 paes_2 <- paes |> 
   select(año = anyo_proceso, 
          cod_sexo, 
-         fecha_nacimiento, 
          codigo_comuna = codigo_comuna_egreso,
          promedio_notas,
          ends_with("actual"),
          -matches("_inv_"),
          -rindio_proceso_actual
-         ) |> 
+  ) |> 
   filter(!is.na(codigo_comuna)) |> 
-  mutate(across(where(is.numeric), ~na_if(.x, 0)))
+  mutate(across(where(is.numeric), ~na_if(.x, 0))) |> 
+  mutate(cod_sexo = case_match(cod_sexo, 
+                               1 ~ "Masculino",
+                               2 ~ "Femenino"))
 
-
-# anexar comunas
-cut_comuna <- readr::read_csv2("https://github.com/bastianolea/cut_comunas/raw/master/datos/cut_comuna.csv") |> 
-  select(codigo_region, nombre_region, codigo_comuna, nombre_comuna) |> 
-  mutate(codigo_comuna = as.numeric(codigo_comuna))
-
+# nombres de variables
 paes_3 <- paes_2 |> 
-  left_join(cut_comuna, by = "codigo_comuna") |> 
-  relocate(nombre_comuna, codigo_comuna, nombre_region, codigo_region, .before = 1)
-
-# guardar base completa
-readr::write_csv2(paes_3, "datos/puntajes_paes_2024.csv")
-arrow::write_parquet(paes_3, "datos/puntajes_paes_2024.parquet")
-
-
-# obtener promedios comunales
-paes_4 <- paes_3 |> 
-  group_by(nombre_comuna, codigo_comuna, nombre_region, codigo_region) |> 
-  summarize(across(c(promedio_notas, ends_with("actual")), ~mean(.x, na.rm = T))) |> 
-  group_by(nombre_comuna, codigo_comuna) |> 
-  mutate(n_comuna = n()) |> 
   rename_with(~stringr::str_remove(.x, "_actual")) |> 
   rename_with(~stringr::str_remove(.x, "_reg")) |> 
   rename(paes_complectora = clec,
@@ -49,6 +32,43 @@ paes_4 <- paes_3 |>
          paes_histciesoc = hcsoc,
          paes_ciencias = cien)
 
+# anexar comunas
+cut_comuna <- readr::read_csv2("https://github.com/bastianolea/cut_comunas/raw/master/datos/cut_comuna.csv") |> 
+  select(codigo_region, nombre_region, codigo_comuna, nombre_comuna) |> 
+  mutate(codigo_comuna = as.numeric(codigo_comuna))
+
+paes_4 <- paes_3 |> 
+  left_join(cut_comuna, by = "codigo_comuna") |> 
+  relocate(nombre_comuna, codigo_comuna, nombre_region, codigo_region, .before = 1)
+
+
+# guardar base completa
+readr::write_csv2(paes_4, "datos/puntajes_paes_2024.csv")
+arrow::write_parquet(paes_4, "datos/puntajes_paes_2024.parquet")
+
+
+# obtener promedios comunales
+paes_5 <- paes_4 |> 
+  group_by(nombre_comuna, codigo_comuna, nombre_region, codigo_region) |> 
+  summarize(
+    # mínimos
+    across(c(promedio_notas, paes_complectora, paes_matematica1, paes_matematica2, paes_histciesoc, paes_ciencias), 
+           ~min(.x, na.rm = T), .names = "{.col}_min"),
+    # máximos
+    across(c(promedio_notas, paes_complectora, paes_matematica1, paes_matematica2, paes_histciesoc, paes_ciencias), 
+           ~max(.x, na.rm = T), .names = "{.col}_max"),
+    # promedios
+    across(c(promedio_notas, paes_complectora, paes_matematica1, paes_matematica2, paes_histciesoc, paes_ciencias), 
+           ~mean(.x, na.rm = T)),
+    # cantidad
+    n_comuna = n()
+    ) |> 
+  mutate(across(c(starts_with("promedio"), starts_with("paes")), ~round(.x, 1))) |> 
+  relocate(n_comuna, promedio_notas, paes_complectora, paes_matematica1, paes_matematica2, paes_histciesoc, paes_ciencias,
+           .after = codigo_region)
+
+glimpse(paes_5)
+
 # guardar base por comunas
-readr::write_csv2(paes_4, "datos/puntajes_paes_comuna_2024.csv")
-arrow::write_parquet(paes_4, "datos/puntajes_paes_comuna_2024.parquet")
+readr::write_csv2(paes_5, "datos/puntajes_paes_comuna_2024.csv")
+arrow::write_parquet(paes_5, "datos/puntajes_paes_comuna_2024.parquet")
